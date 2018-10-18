408aa25f398c069b07740406289e5a71f00da4f9
Mathias <mathiasminacapilli@gmail.com>
2018-10-17 22:54:53 -0300

    Se agregan cosas al archivo intervalos.cpp (se pasa al directorio ppal)

---
+++ intervalos.cpp
@@ -0,0 +1,38 @@
+
+#include "include/intervalos.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <climits>
+#include <time.h>
+
+/* Devuelve un arreglo de booleanos de 'n' con TRUE en los intervalos asignados,
+   los cuales no se superponen.
+   La cantidad de intervalos asignados debe ser la máxima posible.
+   'intervalos' es un arreglo con 'n' intervalo_t.
+   El tiempo de ejecucion de peor caso debe ser O(n*log(n)).
+*/
+bool *max_cantidad(const intervalo_t *intervalos, uint n) {
+    bool *ab[n];
+    return ab[n];
+}
+
+
+/* Devuelve un arreglo de booleanos de 'n' con TRUE en los intervalos asignados,
+   los cuales no se superponen.
+   La suma de los volumenes de los intervalos asignados debe ser la máxima
+   posible.
+   'intervalos' es un arreglo con 'n' intervalo_t.
+    El tiempo de ejecucion de peor caso debe ser O(n*log(n)).
+*/
+bool *max_volumen(const intervalo_t *intervalos, uint n) {
+    bool *ab[n];
+    return ab[n];
+}
+
+
+
+
+
